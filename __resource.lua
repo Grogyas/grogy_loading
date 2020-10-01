66c3f0188e0c43c978958a2c120c142ddb1df32a
Grogy <48384565+Grogyas@users.noreply.github.com>
2020-10-01 19:46:12 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,28 @@
+description 'Grogy-Loading'
+
+files {
+	'index.html',
+
+	'audio/1.mp3',
+	'audio/2.mp3',
+	'audio/3.mp3',
+	'audio/4.mp3',
+	'audio/5.mp3',
+
+
+	'css/styles.css',
+
+	'img/cursor.png',
+	'img/hudba.png',
+	'img/logo.png',
+	'img/bg1.jpg',
+	'img/bg2.jpg',
+	'img/bg3.jpg',
+	'img/bg4.jpg',
+	'img/bg5.jpg',
+	
+	'js/main.js',
+
+}
+
+loadscreen 'index.html'
